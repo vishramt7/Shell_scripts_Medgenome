0e2e3428ef1a407cc679ec6e7a49d5982698b8f1
Vishram Terse <vishram.terse@medgenome.com>
2021-11-19 09:27:58 +0530

    minor changes for copying

--- fig_gen.sh
+++ fig_gen.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash 
 
-for i in $(ls *b37.txt)
+for i in $(ls *UKBB*)
 do 
 	file=$(basename ${i} .txt)
 	echo ${file}
 	Rscript manhattan_modified.R ${file}
+	cp ${file}_manhattan.png ../Manhattan_plots/
 done
